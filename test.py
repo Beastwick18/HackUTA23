43efbf36fc4c87d365c45a3272e383cedfb00548
Steven Culwell <beastwick18@gmail.com>
2023-10-07 17:16:28 -0500

    asdf

--- test.py
+++ test.py
@@ -1,8 +1,17 @@
 import streamlit as st
-from thegsm import test
 import requests
 
+
+def test(id):
+    data = {"id": id}
+    r = requests.post(
+        "https://focus-sequencer-401321.uc.r.appspot.com/api/post_test", json=data)
+
+    return r.content
+
+
 st.title("the game")
+
 
 gaming = st.text_input("give me a number")
 button = st.button("request")
@@ -10,9 +19,3 @@ r = test(gaming)
 
 label = st.text("")
 label.text(r)
-
-def test(id):
-    data = {"id" : id}
-    r = requests.post("https://focus-sequencer-401321.uc.r.appspot.com/api/post_test",json=data)
-    
-    return r.content
