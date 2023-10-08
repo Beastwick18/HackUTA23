3b1660606f68b3cf4b234e5c77f7e220e1cb8e2f
Steven Culwell <beastwick18@gmail.com>
2023-10-08 08:44:21 -0500

    10-08-23 08:43:58

--- pages/api/player_status.ts
+++ pages/api/player_status.ts
@@ -10,7 +10,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     // const result = await db.collection("rooms").find({ id: roomId }).toArray()
     const player = await db.collection("players").find({ roomId: id }).toArray()
     // await db.collection("players").updateOne({ username: username }, { $set: { ready: 1 } });
-    res.status(200).send()
+    res.status(200).send({})
     return;
     //console.log(result)
   } catch (err: any) {
