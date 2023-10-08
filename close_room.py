0b7650e4963c61d4b78b770282d136320f2ba111
AbrahamR176 <abraham.rincon@mavs.uta.edu>
2023-10-07 22:48:32 -0500

    rtr

--- close_room.py
+++ close_room.py
@@ -5,5 +5,5 @@ t= st.text_area("Insert the id of the room")
 s= st.button("Click to close the room")
 id = {"id": t}
 if s:
-    r = requests.post("http://10.138.235.231:3000/api/close_room",json=id)
+    r = requests.post("http://10.183.235.231:3000/api/close_room",json=id)
     print(r.content)
