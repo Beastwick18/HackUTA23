ab82bb7ff2a890ededf16f0f12163c40dd681b03
AbrahamR176 <abraham.rincon@mavs.uta.edu>
2023-10-07 20:48:01 -0500

    xgbgbf

--- camera.py
+++ camera.py
@@ -6,9 +6,9 @@ import json
 picture = st.camera_input("Take a picture")
 
 if picture:
-    #r = requests.post("https://focus-sequencer-401321.uc.r.appspot.com/api/upload_image",picture.getvalue())
     data = base64.b64encode(picture.getvalue())
-    r = requests.post("http://10.183.235.231:3000/api/upload_image", data)
+    r = requests.post("https://focus-sequencer-401321.uc.r.appspot.com/api/upload_image",data)
+    #r = requests.post("http://10.183.235.231:3000/api/upload_image", data)
     
     info = json.loads(r.content)
     ts = ""
