d80b88b996806c7782ed02f2a241089a8d7e2165
AbrahamR176 <abraham.rincon@mavs.uta.edu>
2023-10-07 22:13:48 -0500

    thegame

---
+++ close_room.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import requests
+
+t= st.text_area("Insert the id of the room")
+s= st.button("Click to close the room")
+id = {"id": t}
+if s:
+    r = requests.post("http://10.138.235.231:3000/api/close_room",json=id)
+    print(r.content)
