31216df5935504a2b096bc96b0c18a51bf2459f9
AbrahamR176 <abraham.rincon@mavs.uta.edu>
2023-10-07 20:45:49 -0500

    thga em

--- camera.py
+++ camera.py
@@ -1,6 +1,20 @@
 import streamlit as st
+import requests
+import base64
+import json
 
 picture = st.camera_input("Take a picture")
 
 if picture:
-    st.image(picture)
+    #r = requests.post("https://focus-sequencer-401321.uc.r.appspot.com/api/upload_image",picture.getvalue())
+    data = base64.b64encode(picture.getvalue())
+    r = requests.post("http://10.183.235.231:3000/api/upload_image", data)
+    
+    info = json.loads(r.content)
+    ts = ""
+    
+    for a in info["data"]:
+        ts += (f"{a['description']} {a['score']}\n")
+    
+    st.text(ts)
+    
